3d037ea90369b2dc8d77f7e6ce36b944eeeab156
Matteo Panara <matteo@lanieri.com>
2020-11-08 11:53:33 +0100

    Stylistic linting

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -1,55 +1,54 @@
 
 class TennisGame1
-
-  def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
-    @p1points = 0
-    @p2points = 0
+  def initialize(p1_name, p2_name)
+    @p1_name = p1_name
+    @p2_name = p2_name
+    @p1_points = 0
+    @p2_points = 0
   end
-        
-  def won_point(playerName)
-    if playerName == "player1"
-      @p1points += 1
+
+  def won_point(player_name)
+    if player_name == 'player1'
+      @p1_points += 1
     else
-      @p2points += 1
+      @p2_points += 1
     end
   end
-  
+
   def score
-    result = ""
-    tempScore=0
-    if (@p1points==@p2points)
+    result = ''
+    temp_score = 0
+    if @p1_points == @p2_points
       result = {
-          0 => "Love-All",
-          1 => "Fifteen-All",
-          2 => "Thirty-All",
-      }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
-      if (minusResult==1)
-        result ="Advantage player1"
-      elsif (minusResult ==-1)
-        result ="Advantage player2"
-      elsif (minusResult>=2)
-        result = "Win for player1"
+        0 => 'Love-All',
+        1 => 'Fifteen-All',
+        2 => 'Thirty-All'
+      }.fetch(@p1_points, 'Deuce')
+    elsif @p1_points >= 4 || @p2_points >= 4
+      minus_result = @p1_points - @p2_points
+      if minus_result == 1
+        result = 'Advantage player1'
+      elsif minus_result == -1
+        result = 'Advantage player2'
+      elsif minus_result >= 2
+        result = 'Win for player1'
       else
-        result ="Win for player2"
+        result = 'Win for player2'
       end
     else
       (1...3).each do |i|
-        if (i==1)
-          tempScore = @p1points
+        if i == 1
+          temp_score = @p1_points
         else
-          result+="-"
-          tempScore = @p2points
+          result += '-'
+          temp_score = @p2_points
         end
         result += {
-            0 => "Love",
-            1 => "Fifteen",
-            2 => "Thirty",
-            3 => "Forty",
-        }[tempScore]
+          0 => 'Love',
+          1 => 'Fifteen',
+          2 => 'Thirty',
+          3 => 'Forty'
+        }[temp_score]
       end
     end
     result
