90cd110c852623906614a8ee766da976dc7c3e37
Matteo Panara <matteo@lanieri.com>
2020-11-08 13:22:03 +0100

    Focus on the third implementation

--- ruby/tennis_test.rb
+++ ruby/tennis_test.rb
@@ -62,21 +62,21 @@ class TestTennis < Minitest::Test
     game
   end
 
-  def test_Score_Game1
-    TEST_CASES.each do |testcase|
-      (p1Points, p2Points, score, p1Name, p2Name) = testcase
-      game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
-      assert_equal(score, game.score())
-    end
-  end
+  # def test_Score_Game1
+  #   TEST_CASES.each do |testcase|
+  #     (p1Points, p2Points, score, p1Name, p2Name) = testcase
+  #     game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
+  #     assert_equal(score, game.score())
+  #   end
+  # end
 
-  def test_Score_Game2
-    TEST_CASES.each do |testcase|
-      (p1Points, p2Points, score, p1Name, p2Name) = testcase
-      game = play_game(TennisGame2, p1Points, p2Points, p1Name, p2Name)
-      assert_equal(score, game.score())
-    end
-  end
+  # def test_Score_Game2
+  #   TEST_CASES.each do |testcase|
+  #     (p1Points, p2Points, score, p1Name, p2Name) = testcase
+  #     game = play_game(TennisGame2, p1Points, p2Points, p1Name, p2Name)
+  #     assert_equal(score, game.score())
+  #   end
+  # end
 
   def test_Score_Game3
     TEST_CASES.each do |testcase|
