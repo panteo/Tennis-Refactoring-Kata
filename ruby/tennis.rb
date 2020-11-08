793557cfd0beabf0ee44020a36a41d3c7415b4f4
Matteo Panara <matteo@lanieri.com>
2020-11-08 11:56:56 +0100

    Early returns

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -17,24 +17,18 @@ class TennisGame1
 
   def score
     result = ''
-    temp_score = 0
     if @p1_points == @p2_points
-      result = {
+      return {
         0 => 'Love-All',
         1 => 'Fifteen-All',
         2 => 'Thirty-All'
       }.fetch(@p1_points, 'Deuce')
     elsif @p1_points >= 4 || @p2_points >= 4
       minus_result = @p1_points - @p2_points
-      if minus_result == 1
-        result = 'Advantage player1'
-      elsif minus_result == -1
-        result = 'Advantage player2'
-      elsif minus_result >= 2
-        result = 'Win for player1'
-      else
-        result = 'Win for player2'
-      end
+      return 'Advantage player1' if minus_result == 1
+      return 'Advantage player2' if minus_result == -1
+      return 'Win for player1' if minus_result >= 2
+      return 'Win for player2'
     else
       (1...3).each do |i|
         if i == 1
