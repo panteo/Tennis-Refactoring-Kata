86c7e40170afa0218d4b0a4f60e99d251daea36d
Matteo Panara <matteo@lanieri.com>
2020-11-08 12:00:15 +0100

    Extract code from score method which is too long

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -1,4 +1,3 @@
-
 class TennisGame1
   def initialize(p1_name, p2_name)
     @p1_name = p1_name
@@ -16,34 +15,44 @@ class TennisGame1
   end
 
   def score
+    return score_draw if @p1_points == @p2_points
+    return score_advantage_or_win if @p1_points >= 4 || @p2_points >= 4
+    score_in_progress
+  end
+
+  private
+
+  def score_advantage_or_win
+    minus_result = @p1_points - @p2_points
+    return 'Advantage player1' if minus_result == 1
+    return 'Advantage player2' if minus_result == -1
+    return 'Win for player1' if minus_result >= 2
+    'Win for player2'
+  end
+
+  def score_draw
+    {
+      0 => 'Love-All',
+      1 => 'Fifteen-All',
+      2 => 'Thirty-All'
+    }.fetch(@p1_points, 'Deuce')
+  end
+
+  def score_in_progress
     result = ''
-    if @p1_points == @p2_points
-      return {
-        0 => 'Love-All',
-        1 => 'Fifteen-All',
-        2 => 'Thirty-All'
-      }.fetch(@p1_points, 'Deuce')
-    elsif @p1_points >= 4 || @p2_points >= 4
-      minus_result = @p1_points - @p2_points
-      return 'Advantage player1' if minus_result == 1
-      return 'Advantage player2' if minus_result == -1
-      return 'Win for player1' if minus_result >= 2
-      return 'Win for player2'
-    else
-      (1...3).each do |i|
-        if i == 1
-          temp_score = @p1_points
-        else
-          result += '-'
-          temp_score = @p2_points
-        end
-        result += {
-          0 => 'Love',
-          1 => 'Fifteen',
-          2 => 'Thirty',
-          3 => 'Forty'
-        }[temp_score]
+    (1...3).each do |i|
+      if i == 1
+        temp_score = @p1_points
+      else
+        result += '-'
+        temp_score = @p2_points
       end
+      result += {
+        0 => 'Love',
+        1 => 'Fifteen',
+        2 => 'Thirty',
+        3 => 'Forty'
+      }[temp_score]
     end
     result
   end
